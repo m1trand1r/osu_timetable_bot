9f6febdca22e552bf30422be2d72cb37fb4221a3
m1trand1r <andrey20005647@gmail.com>
2021-08-17 23:59:02 +0500

    preparations for real bot

--- src/server.py
+++ src/server.py
@@ -4,22 +4,29 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-# from config import TOKEN
+from config import TOKEN
 
 # Config string for docker image
-TG_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+# TG_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
-bot = Bot(token=TG_API_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
-
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     markup = types.InlineKeyboardMarkup()
-    student = types.InlineKeyboardButton('Студент')
-    teacher = types.InlineKeyboardButton('Преподаватель')
+    student = types.InlineKeyboardButton('Студент', callback_data='type_student')
+    teacher = types.InlineKeyboardButton('Преподаватель', callback_data='type_teacher')
+    markup.add(*[student, teacher])
+    await bot.send_message(message.chat.id, "Добро пожаловать в бот с расписанием!\n"
+                                            "Выберите студент вы или преподаватель",
+                           reply_markup=markup)
+
+
+@dp.callback_query_handler(lambda call: call.data.startswith('type_'))
+async def choose_faculty(call: types.CallbackQuery):
+    await bot.send_message(call.message.chat.id, f"Вы выбрали {'студент' if call.data == 'type_student' else 'преподаватель'}" )
 
 
 @dp.message_handler()
